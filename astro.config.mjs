45a78c2c10f2c15c32e330fef760b22a5895953a
xsamueljr <samueljiro.alfa@gmail.com>
2025-02-17 21:45:21 +0100

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'xsamueljr.github.io',
+    site: 'https://xsamueljr.github.io',
     base: 'confia'
 });
