fb1243343d254b8d8000f303b08f265b423d787c
xsamueljr <samueljiro.alfa@gmail.com>
2025-02-18 22:50:32 +0100

    fix assets routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 import { IS_PROD } from './src/env';
 
+const GITHUB_SITE = 'https://xsamueljr.github.io/confia';
+
 // https://astro.build/config
 export default defineConfig({
     site: IS_PROD ? 'https://xsamueljr.github.io' : undefined,
-    base: IS_PROD ? 'confia' : undefined
+    base: IS_PROD ? '/confia' : undefined,
+    build: {
+        assetsPrefix: IS_PROD ? '/confia' : undefined
+    }
 });
