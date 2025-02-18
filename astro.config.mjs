1ad796ff3b36e018ff96cf21ac70a061ea9addcf
xsamueljr <samueljiro.alfa@gmail.com>
2025-02-18 22:53:36 +0100

    Revert "configurar entorno de producción y mejorar redirecciones de idioma"
    This reverts commit 6cf5e91c3d385352b859f6e228849cc0484dc9b9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { IS_PROD } from './src/env';
 
 // https://astro.build/config
 export default defineConfig({
-    site: IS_PROD ? 'https://xsamueljr.github.io' : undefined,
-    base: IS_PROD ? 'confia' : undefined
+    site: 'https://xsamueljr.github.io',
+    base: 'confia'
 });
--- src/utils/redirections.ts
+++
@@ -1,9 +0,0 @@
-import { IS_PROD } from '../env';
-
-const BASE_PATH = IS_PROD ? '/confia' : '';
-const getRealPath = (path: string): string => `${BASE_PATH}/${path}`;
-
-export const PATH_TO = {
-    EN: getRealPath('en'),
-    ES: getRealPath('es'),
-} as const;
